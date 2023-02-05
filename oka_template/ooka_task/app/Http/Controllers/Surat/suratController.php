1b94cb96437205a53386cb6a2538ff4c882d2c81
zabil sabri muhammad <zabilsabrimuhammad0@gmail.com>
2023-02-05 22:46:14 +0800

    Template surat dompdf (on process)

---
+++ oka_template/ooka_task/app/Http/Controllers/Surat/suratController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Surat;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use PDF;
+
+class suratController extends Controller
+{
+    public function exportData($id)
+    {
+        switch($id){
+            case 1:
+                $pdf = Pdf::loadView('surat/isiSurat/domisili');
+                break;
+            case 2:
+                $pdf = Pdf::loadView('surat/isiSurat/belumNikah');
+                break;
+            case 3:
+                $pdf = Pdf::loadView('surat/isiSurat/sku');
+                break;
+            case 4:
+                $pdf = Pdf::loadView('surat/isiSurat/kurangMampu');
+                break;
+            case 5:
+                $pdf = Pdf::loadView('surat/isiSurat/ketKematian');
+                break;
+
+        }
+        $pdf->setPaper('A4', 'potrait');
+        return $pdf->stream('test.pdf' , array("Attachment" => false));
+        exit(0);
+    }
+}
