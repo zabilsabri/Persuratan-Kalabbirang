d379d66f634374cf480d811427090cf0b96598c1
ookapratama <ookapratama2@gmail.com>
2023-02-21 03:22:13 +0800

    buat api suratMasuk

--- oka_template/ooka_task/app/Http/Controllers/Auth/LoginController.php
+++ oka_template/ooka_task/app/Http/Controllers/Auth/LoginController.php
@@ -10,75 +10,9 @@ use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
-    // public function login() {
-    //     return view('auth.login');
-    // }
-
-    public function login(Request $request)
-    {   
-        // $tes = encrypt('tes');
-        // dump($tes);
-        // $dec = decrypt($tes);
-        // dd($dec);
-        // dd();
-        $user = User::where('nik', $request->nik)->first();
-        if ($request->nik == $user->nik && $request->nomor_telp == $user->nomor_telp) {
-            return response()->json([
-                'status' => 'Berhasil'
-            ], 200);
-        } else {
-            return response()->json([
-                'status' => 'Gagal'
-            ], 401);
-        }
+    public function login() {
+        return view('auth.login');
     }
-    
-    public function register(Request $request) {
-        $validate = Validator::make($request->all(), [
-            'nama'                      => 'required',
-            // 'password'                  => 'required',
-            'username'                  => 'required',
-            'nik'                       => 'required',
-            'ttl'                       => 'required',
-            'jkl'                       => 'required',
-            'alamat'                    => 'required',
-            'pekerjaan'                 => 'required',
-            'nomor_kk'                     => 'required',
-            'nomor_telp'                   => 'required',
-            'profil'                    => 'required',
-            'ktp'                       => 'required',
-            'email'                     => 'required',
-        ]);
-        
-        if ($validate->fails()) {
-            return response()->json($validate->errors());
-        }
-        
-        
-        $register = User::create([
-            'nama'                      => $request->nama,
-            // 'password'                  => encrypt($request->password),
-            'username'                  => $request->username,
-            'nik'                       => $request->nik,
-            'ttl'                       => Carbon::now(),
-            'jkl'                       => $request->jkl,
-            'alamat'                    => $request->alamat,
-            'pekerjaan'                 => $request->pekerjaan,
-            'nomor_kk'                     => $request->nomor_kk,
-            'nomor_telp'                   => $request->nomor_telp,
-            'profil'                    => $request->profil,
-            'ktp'                       => $request->ktp,
-            'email'                     => $request->email,
-        ]); 
 
-        if ($register)
-        return response()->json([
-            'status' => 'Sukses Register'
-        ]);
-
-        return response()->json([
-            'status' => 'Gagal Register'
-        ]);
-    }
 
 }
