f48f7b46f9d39956f96709afeea36ab651f8762b
zabilsabri <zabilsabrimuhammad0@gmail.com>
2023-02-21 10:49:11 +0800

    Perbarui Database

--- oka_template/ooka_task/database/migrations/2014_10_12_000000_create_users_table.php
+++ oka_template/ooka_task/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,17 +18,19 @@ return new class extends Migration
             $table->string('nama');
             $table->string('username');
             $table->string('nik')->unique();
-            $table->date('ttl');
+            $table->date('ttl')->nullable();
             $table->enum('jkl', ['laki-laki', 'perempuan']);
-            $table->string('alamat');
-            $table->string('pekerjaan');
+            $table->string('alamat')->nullable();;
+            $table->string('role');
+            $table->string('pekerjaan')->nullable();
             $table->string('nomor_kk');
             $table->string('nomor_telp');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('profil'); // fotot profil
+            $table->string('password')->nullable();
+            $table->string('profil')->nullable(); // foto profil
             $table->string('ktp'); // foto kk
+            $table->string('swafoto_ktp'); // foto kk
             $table->rememberToken();
             $table->timestamps();
         });
