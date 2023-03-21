9dff9136c8967a2a73a23ba70a97a20c7520bef5
zabilsabri <zabilsabrimuhammad0@gmail.com>
2023-03-21 18:20:46 +0800

    Perbarui database & seeder

--- oka_template/ooka_task/database/migrations/2014_10_12_000000_create_users_table.php
+++ oka_template/ooka_task/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,17 +15,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('role_id');
             $table->string('nama');
             $table->string('username');
             $table->string('nik')->unique();
-            $table->date('ttl')->nullable()->format('d-m-Y');
-            $table->enum('jkl', ['laki-laki', 'perempuan'])->nullable();
+            $table->date('tanggal_lahir')->nullable();
+            $table->string('tempat_lahir')->nullable();
+            $table->string('status_nikah')->nullable();
+            $table->string('kewarganegaraan')->nullable();
+            $table->enum('agama', ['Islam', 'Kristen', 'Hindu', 'Buddha', 'Katolik'])->nullable();
+            $table->enum('jkl', ['Laki - Laki', 'Perempuan'])->nullable();            
             $table->string('alamat')->nullable();
             $table->string('pekerjaan')->nullable();
             $table->string('nomor_kk');
             $table->string('nomor_telp');
-            $table->string('email')->unique();
+            $table->string('email')->unique();  
             $table->timestamp('email_verified_at')->nullable();
             // $table->string('password');
             $table->string('profil')->nullable(); // foto profil
