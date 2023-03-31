435341f71b489f082dccf51b346c38105ad2065d
zabilsabri <zabilsabrimuhammad0@gmail.com>
2023-03-31 21:23:22 +0800

    Perbarui Database

--- oka_template/ooka_task/database/migrations/2023_02_06_031117_create_surat_masuks_table.php
+++ oka_template/ooka_task/database/migrations/2023_02_06_031117_create_surat_masuks_table.php
@@ -16,8 +16,10 @@ return new class extends Migration
         Schema::create('surat_masuks', function (Blueprint $table) {
             $table->id();
             $table->string('asal_surat');
+            $table->string('judul_surat');
             $table->string('no_surat');
             $table->string('tujuan_surat');
+            $table->string('jenis_surat');
             $table->enum('status', ['Rahasia', 'Penting', 'Segera', 'Biasa']);
             $table->string('file_surat');
             $table->timestamps();
